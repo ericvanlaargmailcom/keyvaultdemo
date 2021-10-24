b2060144e208817f00d01eaa7b2112fffabe27a8
Eric van Laar <eric.vanlaar@gmail.com>
2021-10-24 20:42:20 +0200

    outcommented everything

--- go.ps1
+++ go.ps1
@@ -1,3 +1,3 @@
 # New-AzResourceGroup -Name rg-keyvaultdeploy-demo-001 -Location westeurope
 # New-AzResourceGroupDeployment -ResourceGroupName rg-keyvaultdeploy-demo-001 -TemplateFile ./deploy.json -TemplateParameterFile ./deployparam.json
-Remove-AzResourceGroup -Name rg-keyvaultdeploy-demo-001 -Force
+# Remove-AzResourceGroup -Name rg-keyvaultdeploy-demo-001 -Force
