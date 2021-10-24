c39b4d06b93b10d3beccbd6f531430ed8cb6fdd7
Eric van Laar <eric.vanlaar@gmail.com>
2021-10-24 21:58:25 +0200

    com

--- go.ps1
+++ go.ps1
@@ -1,3 +1,3 @@
 # New-AzResourceGroup -Name rg-keyvaultdeploy-demo-001 -Location westeurope
-# New-AzResourceGroupDeployment -ResourceGroupName rg-keyvaultdeploy-demo-001 -TemplateFile ./deploy.json -TemplateParameterFile ./deployparam.json
+New-AzResourceGroupDeployment -ResourceGroupName rg-keyvaultdeploy-demo-001 -TemplateURI ./deploy.json -TemplateParameterFile ./deployparam.json
 # Remove-AzResourceGroup -Name rg-keyvaultdeploy-demo-001 -Force
