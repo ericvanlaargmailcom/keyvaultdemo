a2fff61b8048fb229fc793353ca1c1e255da43a1
Eric <eric.vanlaar@gmail.com>
2021-11-03 13:18:08 +0100

    go

--- go.ps1
+++ go.ps1
@@ -1,3 +1,9 @@
-# New-AzResourceGroup -Name rg-keyvaultdeploy-demo-001 -Location westeurope
-New-AzResourceGroupDeployment -ResourceGroupName rg-keyvaultdeploy-demo-001 -TemplateURI ./deploy.json -TemplateParameterFile ./deployparam.json
-# Remove-AzResourceGroup -Name rg-keyvaultdeploy-demo-001 -Force
+# New-AzResourceGroup -Name rg-keyvaultdeployed-demo-001 -Location westeurope -verbose
+
+# New-AzResourceGroupDeployment `
+# -ResourceGroupName rg-keyvaultdeployed-demo-001 `
+# -TemplateURI ./deploy.json `
+# -TemplateParameterFile ./deployparam.json `
+# -Verbose
+
+Remove-AzResourceGroup -Name rg-keyvaultdeployed-demo-001 -Force -verbose
