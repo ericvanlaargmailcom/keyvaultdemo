511e73310615b0e18563589a59f9be113c50e5ec
Eric <eric.vanlaar@gmail.com>
2021-11-03 13:26:12 +0100

    rare toevoeging weer weggehaald

--- go.ps1
+++ go.ps1
@@ -7,5 +7,3 @@ New-AzResourceGroupDeployment `
 -Verbose
 
 # Remove-AzResourceGroup -Name rg-keyvaultdeployed-demo-001 -Force -verbose
-
-# een heeeeele rare toevoeging wat moet deze functie in de code????
