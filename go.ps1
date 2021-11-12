f00b7f3ec15c67b227b820c2d2780c37ce488510
Eric <eric.vanlaar@gmail.com>
2021-11-12 22:23:18 +0100

    added line break before removal

--- go.ps1
+++ go.ps1
@@ -1,12 +1,17 @@
 $msdnsub = "9c108632-3066-4862-b0db-b34e82ba633d"
+$rg = "rg-keyvaultdeployed-demo-001"
+$location = "westeurope"
+
 Set-AzContext -Subscription $msdnsub
 
-New-AzResourceGroup -Name rg-keyvaultdeployed-demo-001 -Location westeurope -verbose
+New-AzResourceGroup -Name $rg -Location $location -verbose
 
 New-AzResourceGroupDeployment `
--ResourceGroupName rg-keyvaultdeployed-demo-001 `
+-ResourceGroupName $rg `
 -TemplateURI ./deploy.json `
 -TemplateParameterFile ./deployparam.json `
 -Verbose
 
-# Remove-AzResourceGroup -Name rg-keyvaultdeployed-demo-001 -Force -verbose
+read-host "Press enter to cleanup the demo"
+Remove-AzResourceGroup -Name NetworkWatcherRG -Force -verbose
+Remove-AzResourceGroup -Name rg-keyvaultdeployed-demo-001 -Force -verbose
