eb061a9cc9613bf4e3898ce9367d0382c54a2c7c
Eric <eric.vanlaar@gmail.com>
2021-11-18 15:30:05 +0100

    changed region to north europe

--- go.ps1
+++ go.ps1
@@ -1,6 +1,6 @@
 $msdnsub = "9c108632-3066-4862-b0db-b34e82ba633d"
 $rg = "rg-keyvaultdeployed-demo-001"
-$location = "westeurope"
+$location = "northeurope"
 
 Set-AzContext -Subscription $msdnsub
 
