5ca15377ce9f4e9e51caf33c4f3f062496f0ca7f
Eric <eric.vanlaar@gmail.com>
2021-11-26 15:20:28 +0100

    added three  rules for demo

--- go.ps1
+++ go.ps1
@@ -15,3 +15,6 @@ New-AzResourceGroupDeployment `
 read-host "Press enter to cleanup the demo"
 Remove-AzResourceGroup -Name NetworkWatcherRG -Force -verbose
 Remove-AzResourceGroup -Name $rg -Force -verbose
+
+Set-AzContext -Subscription "9c108632-3066-4862-b0db-b34e82ba633d"
+## this is a new line
