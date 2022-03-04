6e6c283ef44b6c523224718f4f6c2c6d4826d37c
Eric van Laar <eric.vanlaar@gmail.com>
2022-03-04 21:33:54 +0100

    nogeenkeer

--- go.ps1
+++ go.ps1
@@ -1,8 +1,13 @@
+$Tenant = "8c781dba-1521-44e4-9886-e37f91a6f736"
 $SubID = "9c108632-3066-4862-b0db-b34e82ba633d"
 $rgName = "rg-keyvaultdeployed-demo-001"
 $Location = "northeurope"
 
 Set-AzContext -Subscription $SubID
+
+if ([string]::IsNullOrEmpty($(Get-AzContext).Account)) {Connect-AzAccount}
+#read-host "Press enter to login to Traineraccount & MSDN subscription"
+#Connect-AzAccount -Tenant $Tenant -SubscriptionId $SubID
 
 New-AzResourceGroup -Name $rgName -Location $Location -verbose
 
@@ -16,6 +21,4 @@ read-host "Press enter to cleanup the demo"
 Remove-AzResourceGroup -Name NetworkWatcherRG -Force -verbose
 Remove-AzResourceGroup -Name $rgName -Force -verbose
 
-# Set-AzContext -Subscription "9c108632-3066-4862-b0db-b34e82ba633d"
-## this is a new line
-## dit lijntje erbij
+
