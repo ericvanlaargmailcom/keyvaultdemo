15d036c57ba005158ede4583994ab91a3fd76358
Eric Laar van <33876752+ericvanlaargmailcom@users.noreply.github.com>
2022-03-04 16:17:49 +0100

    Update changeLApw.ps1

--- changeLApw.ps1
+++ changeLApw.ps1
@@ -1,7 +1,7 @@
 $SubID = "9c108632-3066-4862-b0db-b34e82ba633d"
 $rgName = "rg-keyvaultdeployed-demo-001" 
-$VmName = "" 
-$Location = "<LOCATION>" 
+$VmName = "keyvault-vm" 
+$Location = "northeurope" 
 
 Connect-AzAccount 
 Select-AzSubscription -SubscriptionId $SubID 
