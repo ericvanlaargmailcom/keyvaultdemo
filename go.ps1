9203b78d48f50b9fbaba3edcadd7e55470b86841
Eric Laar van <33876752+ericvanlaargmailcom@users.noreply.github.com>
2022-03-04 16:16:09 +0100

    Update go.ps1

--- go.ps1
+++ go.ps1
@@ -1,20 +1,20 @@
 $SubID = "9c108632-3066-4862-b0db-b34e82ba633d"
-$rg = "rg-keyvaultdeployed-demo-001"
+$rgName = "rg-keyvaultdeployed-demo-001"
 $location = "northeurope"
 
 Set-AzContext -Subscription $SubID
 
-New-AzResourceGroup -Name $rg -Location $location -verbose
+New-AzResourceGroup -Name $rgName -Location $location -verbose
 
 New-AzResourceGroupDeployment `
--ResourceGroupName $rg `
+-ResourceGroupName $rgName `
 -TemplateURI ./deploy.json `
 -TemplateParameterFile ./deployparam.json `
 -Verbose
 
 read-host "Press enter to cleanup the demo"
 Remove-AzResourceGroup -Name NetworkWatcherRG -Force -verbose
-Remove-AzResourceGroup -Name $rg -Force -verbose
+Remove-AzResourceGroup -Name $rgName -Force -verbose
 
 Set-AzContext -Subscription "9c108632-3066-4862-b0db-b34e82ba633d"
 ## this is a new line
