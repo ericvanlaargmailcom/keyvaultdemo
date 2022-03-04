a14b585a1d88d56b38cb559fd4bdc9de59571b98
Eric van Laar <eric.vanlaar@gmail.com>
2022-03-04 16:42:30 +0100

    shorten

---
+++ chpw.ps1
@@ -0,0 +1,8 @@
+$SubID = "9c108632-3066-4862-b0db-b34e82ba633d"
+$rgName = "rg-keyvaultdeployed-demo-001" 
+$VmName = "keyvault-vm" 
+$Location = "northeurope" 
+
+Connect-AzAccount 
+Select-AzSubscription -SubscriptionId $SubID -Verbose
+Set-AzVMAccessExtension -ResourceGroupName $RgName -Location $Location -VMName $VmName -Credential (get-credential) -typeHandlerVersion "2.0" -Name VMAccessAgent -Verbose
--- go.ps1
+++ go.ps1
@@ -16,6 +16,6 @@ read-host "Press enter to cleanup the demo"
 Remove-AzResourceGroup -Name NetworkWatcherRG -Force -verbose
 Remove-AzResourceGroup -Name $rgName -Force -verbose
 
-Set-AzContext -Subscription "9c108632-3066-4862-b0db-b34e82ba633d"
+# Set-AzContext -Subscription "9c108632-3066-4862-b0db-b34e82ba633d"
 ## this is a new line
 ## dit lijntje erbij
