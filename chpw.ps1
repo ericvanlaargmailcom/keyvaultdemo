3cfa6843d9b08a3c8c509cacdc5ce7062e1aaaa8
Eric van Laar <eric.vanlaar@gmail.com>
2022-03-04 20:09:08 +0100

    yes
    Signed-off-by: Eric van Laar <eric.vanlaar@gmail.com>

--- chpw.ps1
+++ chpw.ps1
@@ -3,6 +3,5 @@ $rgName = "rg-keyvaultdeployed-demo-001"
 $VmName = "keyvault-vm" 
 $Location = "northeurope" 
 
-Connect-AzAccount 
 Select-AzSubscription -SubscriptionId $SubID -Verbose
 Set-AzVMAccessExtension -ResourceGroupName $RgName -Location $Location -VMName $VmName -Credential (get-credential) -typeHandlerVersion "2.0" -Name VMAccessAgent -Verbose
--- multideploy.ps1
+++ multideploy.ps1
@@ -1,6 +1,9 @@
 param([string]$resourceGroup)
 $adminCredential = Get-Credential -Message "Enter a username and password for the VM administrator."
 
+$SubID = "9c108632-3066-4862-b0db-b34e82ba633d"
+$rgName = "rg-keyvaultdeployed-demo-001"
+
 For ($i = 1; $i -le 3; $i++) 
 {
 
