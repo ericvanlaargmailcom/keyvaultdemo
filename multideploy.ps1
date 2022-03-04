ee48d19d06bad718bc071c66cead2c9157f91f2e
Eric Laar van <33876752+ericvanlaargmailcom@users.noreply.github.com>
2022-03-04 19:51:16 +0100

    create 3 vm's

---
+++ multideploy.ps1
@@ -0,0 +1,13 @@
+param([string]$resourceGroup)
+$adminCredential = Get-Credential -Message "Enter a username and password for the VM administrator."
+
+For ($i = 1; $i -le 3; $i++) 
+{
+
+}
+
+$vmName = "ConferenceDemo" + $i
+$resourceGroup = "rgconfdemo"
+Write-Host "Creating VM: " $vmName
+
+New-AzVm -ResourceGroupName $resourceGroup -Name $vmName -Credential $adminCredential -Image UbuntuLTS
