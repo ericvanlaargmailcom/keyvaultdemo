0e6df1cc48010c346ed60714ee5baad3cada0831
Eric van Laar <eric.vanlaar@gmail.com>
2022-03-04 21:58:06 +0100

    doit

--- go.ps1
+++ go.ps1
@@ -3,14 +3,13 @@ $SubID = "9c108632-3066-4862-b0db-b34e82ba633d"
 $rgName = "rg-keyvaultdeployed-demo-001"
 $Location = "northeurope"
 
-Set-AzContext -Subscription $SubID
-
-if ([string]::IsNullOrEmpty($(Get-AzContext).Account)) {Connect-AzAccount}
+if ([string]::IsNullOrEmpty($(Get-AzContext).Account)) {Connect-AzAccount -Tenant $Tenant -SubscriptionId $SubID}
 #read-host "Press enter to login to Traineraccount & MSDN subscription"
 #Connect-AzAccount -Tenant $Tenant -SubscriptionId $SubID
 
-New-AzResourceGroup -Name $rgName -Location $Location -verbose
+Set-AzContext -Subscription $SubID
 
+New-AzResourceGroup -Name $rgName -Location $Location -verbose
 New-AzResourceGroupDeployment `
 -ResourceGroupName $rgName `
 -TemplateURI ./deploy.json `
