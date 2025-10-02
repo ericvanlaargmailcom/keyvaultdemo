564bad7f15eceab779582c1ea0e126987b9809e5
Eric van Laar <eric.vanlaar@gmail.com>
2025-10-02 12:08:09 +0200

    df

--- go.ps1
+++ go.ps1
@@ -1,7 +1,7 @@
 $Tenant = "8c781dba-1521-44e4-9886-e37f91a6f736"
 $SubID = "ef17a603-a390-49bd-946c-cc18aa67f388"
 # $PaygoSubID = "af86d54a-4949-4bb5-997d-9dd0f288655a"
-$rgName = "rg_keyvaultdeployed_demo_001"
+$rgName = "rg_vscodedepl_keyvaultvm_demo_001"
 $Location = "northeurope"
 
 if ([string]::IsNullOrEmpty($(Get-AzContext).Account)) {Connect-AzAccount -Tenant $Tenant -SubscriptionId $SubID}
